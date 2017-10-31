23ed39ae6b9ed70e4bae8e40e1a6621adadcf64c
Carson <chake@udel.edu>
2017-10-31 12:16:14 -0400

    added moving background

--- src/models/Arena.java
+++ src/models/Arena.java
@@ -1,22 +1,77 @@
 package models;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+
 public class Arena {
 
 	private final int arenaWidth = 100;
-	private final int arenaHeight = 100;
-	private int enemyCount;
+	private final int arenaHeight = 300;
+	private int poisitionX;
+	private int positionY;
+	private BufferedImage image;
+	
+	
+	private int enemy;
 	private int foodCount;
 	
-	public Arena() {
-		
+	public Arena(String file_name) {
+		this.setPositionY(0);
+		this.setPoisitionX(0);
+		try{
+			File imageFile = new File(file_name);
+			setImage(ImageIO.read(imageFile));
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
-
+	
+	
 	public int getArena_width() {
 		return arenaWidth;
 	}
 
 	public int getArena_height() {
 		return arenaHeight;
+	}
+
+
+	public BufferedImage getImage() {
+		return image;
+	}
+
+
+	public void setImage(BufferedImage image) {
+		this.image = image;
+	}
+
+
+	public int getPoisitionX() {
+		return poisitionX;
+	}
+
+
+	protected void setPoisitionX(int poisitionX) {
+		this.poisitionX = poisitionX;
+	}
+
+	public void updatePosition(){
+		this.poisitionX += 1;
+	}
+	
+	public int getPositionY() {
+		return positionY;
+	}
+
+
+	protected void setPositionY(int positionY) {
+		this.positionY = positionY;
 	}
 
 }
--- src/views/View.java
+++ src/views/View.java
@@ -1,21 +1,39 @@
 package views;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
 import models.Animal;
+import models.Arena;
 
 public class View extends JPanel{
 	
 	public JFrame frame = new JFrame();
 	public Animal viewModel;
+	public ArrayList<Arena> arenas = null;
+	private final String backgroundFolder = "/backgrounds";
+	
+	private int backgroundPosition;
+		
+	private JLayeredPane layeredPane;
 	
 	Component mouseClick = new MyComponent();
 	
@@ -52,8 +70,6 @@ public class View extends JPanel{
 
 		@Override
 		public void mouseDragged(MouseEvent e) {
-			System.out.println(e.getX());
-			
 			viewModel.setXPosition(e.getX());
 			viewModel.setYPosition(e.getY());
 			
@@ -68,23 +84,47 @@ public class View extends JPanel{
 	}
 	
 	public View(Animal animalModel) {
+		super();
 		frame.getContentPane().add(this);
 		frame.setBackground(Color.GRAY);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(animalModel.frameWidth, animalModel.frameHeight);
 		frame.addMouseListener((MouseListener) mouseClick);
 		frame.addMouseMotionListener((MouseMotionListener) mouseClick);
-		frame.setVisible(true);
+		
 		viewModel = animalModel;
+		getArenas(); //does nothing for now
+		frame.setVisible(true);
+	}
+	
+	private void getArenas(){
+//		for(String imageFile: new File(backgroundFolder).list())
+//				arenas.add(new Arena(imageFile));		
+	}
+	
+	public void updateArenas(){
+//		for(Arena ar: arenas)
+//			ar.updatePosition();
+		backgroundPosition = (backgroundPosition > frame.getWidth()) ? 0 : backgroundPosition + 10;		
 	}
 	
 	public void updateViewModel(Animal newViewModel) {
 		this.viewModel = newViewModel;
 	}
 	
+		
 	@Override
 	public void paint(Graphics g){
+		super.paint(g);
+		g.drawImage(new Arena("b1.png").getImage(), backgroundPosition, 0, this);
+		g.drawImage(new Arena("b2.png").getImage(), -backgroundPosition, 0, this);
+		g.drawImage(new Arena("b3.png").getImage(), 200, 0, this);
+		g.drawImage(new Arena("b4.png").getImage(), 210, 0, this);
+		g.drawImage(new Arena("b5.png").getImage(), 310, 0, this);
 		g.drawImage(viewModel.sprite, viewModel.getXPosition(), viewModel.getYPosition(), Color.gray, this);
+		//if(arenas.isEmpty()) return;
+//		for(Arena currentArena: arenas)
+//			g.drawImage(currentArena.getImage(), currentArena.getPoisitionX(), currentArena.getPositionY(), this);
 	}
 	
 
