d36198ce777047e284189ac81118c263e126253e
RJ Bowen <rjbowen@udel.edu>
2017-11-20 17:47:01 -0500

    Added hitbox class to be used

---
+++ src/models/Hitbox.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Hitbox {
+	private int startCornerXPosition;
+	private int startCornerYPosition;
+	private int endCornerXPosition;
+	private int endCornerYPosition;
+	
+	public Hitbox(GameModel x) {
+		
+	}
+	
+}
