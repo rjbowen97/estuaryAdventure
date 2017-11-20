6e97d3dc0e94a5ce6670e63c2c0ad24e2c88ebb2
RJ Bowen <rjbowen@udel.edu>
2017-11-19 21:05:27 -0500

    Land animal implemented successfully

---
+++ src/models/LandAnimal.java
@@ -0,0 +1,23 @@
+package models;
+
+import java.awt.event.MouseEvent;
+
+import controller.Settings;
+
+public class LandAnimal extends Player {
+
+	@Override
+	public void onTick() {
+	}
+	
+	@Override
+	public void onViewClicked(MouseEvent mouseEvent) {
+		if (mouseEvent.getY() > this.yPosition) {
+			this.yPosition += Settings.getLandAnimalShuffleSpeed();
+		}
+		
+		else {
+			this.yPosition -= Settings.getLandAnimalShuffleSpeed();
+		}
+	}	
+}
