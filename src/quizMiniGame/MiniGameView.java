ec116155519b20dcb29986160db151f27f3321c1
RJ Bowen <rjbowen@udel.edu>
2017-11-21 00:33:37 -0500

    Progress

--- src/quizMiniGame/MiniGameView.java
+++ src/quizMiniGame/MiniGameView.java
@@ -2,6 +2,7 @@ package quizMiniGame;
 
 import java.awt.Graphics;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -16,13 +17,19 @@ public class MiniGameView extends JFrame {
 		
 		this.setBounds(0,0,Settings.getViewDimensionDefault(), Settings.getViewDimensionDefault());
 		this.getContentPane().add(new MiniGamePanel());
+		this.pack();
 		this.setVisible(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
 	private class MiniGamePanel extends JPanel {
 		
+		
+		JButton answerAButton = new JButton(miniGame.getCurrentQuestionAndAnswerPair().answer);
+		
 		public MiniGamePanel() {
+			this.add(answerAButton);
+			answerAButton.setSize(1000,1000);
 			this.setBounds(0,0,Settings.getViewDimensionDefault(), Settings.getViewDimensionDefault());
 		}
 		
