1bb50a7ac219367507c18388de7b34d0b82a4b2d
RJ Bowen <rjbowen@udel.edu>
2017-11-21 20:51:05 -0500

    Made a HUD screen to display the current score and health

---
+++ src/views/HUDPane.java
@@ -0,0 +1,44 @@
+package views;
+
+import java.awt.Graphics;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import controller.Settings;
+import models.Player;
+
+public class HUDPane extends JPanel{
+	
+	private Player playerModel;
+	
+	private JLabel healthLabel;
+	private JLabel scoreLabel;
+	
+	
+	public HUDPane(Player playerModel) {
+		this.playerModel = playerModel;
+		
+		int mainFrameDimension = Settings.getViewDimensionDefault();
+		
+		this.setBounds(0, 0, mainFrameDimension, mainFrameDimension);
+		
+		this.healthLabel = new JLabel("HEALTH: " + this.playerModel.getHealth());
+		this.scoreLabel = new JLabel("SCORE: " + this.playerModel.getScore());
+				
+				
+		this.add(healthLabel);
+		this.add(scoreLabel);
+		
+		this.setOpaque(false);
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		this.healthLabel.setText("HEALTH: " + this.playerModel.getHealth());
+		this.scoreLabel.setText("SCORE: " + this.playerModel.getScore());
+		
+	}
+	
+	
+}
--- src/views/MainLayeredPane.java
+++ src/views/MainLayeredPane.java
@@ -15,15 +15,18 @@ public class MainLayeredPane extends JLayeredPane {
 	private PlayerComponent playerComponent;
 	private BackgroundLayeredPane backgroundLayeredPane;
 	private InteractableComponent interactableComponent;
+	private HUDPane hudPane;
 	
 	public MainLayeredPane(Player playerModel, ArrayList<Background> backgroundModels, Controller controller, ArrayList<Interactable> interactableModels) {
 		this.backgroundLayeredPane = new BackgroundLayeredPane(backgroundModels);
 		this.playerComponent = new PlayerComponent(playerModel, controller);
 		this.interactableComponent = new InteractableComponent(interactableModels);
+		this.hudPane = new HUDPane(playerModel);
 		
 		this.add(backgroundLayeredPane, new Integer(0));
 		this.add(interactableComponent, new Integer(1));
 		this.add(playerComponent, new Integer(2));
+		this.add(hudPane, new Integer(3));
 		
 		this.setBounds(0,0,Settings.getViewDimensionDefault(), Settings.getViewDimensionDefault());
 		this.setVisible(true);
