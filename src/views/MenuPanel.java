e65ce1cd95b8b6226dce6893c4d92aea68eebc73
Carson <chake@udel.edu>
2017-12-05 19:15:53 -0500

    improving menu deisgn

--- src/views/MenuPanel.java
+++ src/views/MenuPanel.java
@@ -15,13 +15,24 @@ import javax.swing.JTextField;
 import controller.Controller;
 import controller.Settings;
 import models.Menu;
+import oracle.jrockit.jfr.parser.ParseException;
+
+import javax.swing.BoxLayout;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.ButtonGroup;
+import java.awt.FlowLayout;
+import java.awt.Component;
+import java.awt.Rectangle;
+import javax.swing.JFormattedTextField;
+import javax.swing.JFormattedTextField.AbstractFormatter;
 
 public class MenuPanel extends JPanel implements ActionListener, Serializable  {
 
 	private Controller controller;
 	private Menu menu;
 	
-	JTextField nameField;
+	JFormattedTextField nameField;
 	JButton setNameButton;
 	
 	JLabel difficultyLabel = new JLabel("Difficulty");
@@ -37,43 +48,70 @@ public class MenuPanel extends JPanel implements ActionListener, Serializable  {
 	JButton hardButton;
 	
 	JButton quitButton;
+	private final ButtonGroup actionButtons = new ButtonGroup();
 
 	public MenuPanel(Menu menu, Controller controller) {
 		this.menu = menu;
 		this.controller = controller;
 		
 		Settings set = new Settings();
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 		
 		nameLabel = new JLabel("Name:");
-		nameLabel.setBounds(Settings.getViewDimensionXDefault()/2, Settings.getViewDimensionXDefault()/2, 10, 10);
+		nameLabel.setAlignmentY(Component.TOP_ALIGNMENT);
+		nameLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		nameLabel.setBackground(UIManager.getColor("Label.background"));
+
 		//label it
 		this.add(nameLabel);
-		this.nameField = new JTextField(20);
-		nameField.setBounds(this.getWidth()/2+10, this.getHeight()/2, 10, 10);
-		
+		this.nameField = new JFormattedTextField();
+		nameField.setAlignmentY(10.0f);
+		nameField.setAlignmentX(10.0f);
+		nameField.setToolTipText("enter your name here");
+		nameField.setHorizontalAlignment(SwingConstants.LEFT);
+		AbstractFormatter formatter = nameField.getFormatter();
+		if(formatter != null){
+			String username = nameField.getText();
+			try{
+				formatter.stringToValue(username);
+			} catch (java.text.ParseException e) {
+				e.printStackTrace();
+			}
+		}
 		
 		this.setNameButton = new JButton("Set name");
+		setNameButton.setHorizontalAlignment(SwingConstants.LEFT);
+		setNameButton.setAlignmentY(Component.TOP_ALIGNMENT);
+		actionButtons.add(setNameButton);
 		this.setNameButton.setActionCommand("setName");
 		setNameButton.addActionListener(this);
 		
 		this.playButton = new JButton("Play!");
-		playButton.setBounds(Settings.getViewDimensionXDefault()-20, Settings.getViewDimensionYDefault()-20, 10,10);
+		playButton.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+		actionButtons.add(playButton);
 		
 		playButton.setActionCommand("play");
 		playButton.addActionListener(this);
 		
-		this.quitButton = new JButton("quit");
-		playButton.setBounds(0, 0, 10, 10);
+		this.quitButton = new JButton("Quit");
+		quitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		quitButton.setBounds(new Rectangle(100, 40, 20, 20));
+		quitButton.setHorizontalAlignment(SwingConstants.LEFT);
+		quitButton.setVerticalAlignment(SwingConstants.TOP);
+		actionButtons.add(quitButton);
 		this.quitButton.addActionListener(this);
-
-		
-		this.add(difficultyLabel);
 		this.add(nameField);
+				difficultyLabel.setVerticalTextPosition(SwingConstants.BOTTOM);
+				difficultyLabel.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+				difficultyLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+				difficultyLabel.setVerticalAlignment(SwingConstants.TOP);
+		
+				
+				this.add(difficultyLabel);
 		this.add(setNameButton);
 		this.add(this.playButton);
 		this.add(this.quitButton);
 		
-		this.setBounds(0, 0, Settings.getViewDimensionXDefault(), Settings.getViewDimensionYDefault());
 		this.setVisible(true);
 	}
 	@Override
